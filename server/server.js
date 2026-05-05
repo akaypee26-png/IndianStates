f9e9ff2044045ec6bf57b4430205fac6d7c8e0cd
Kalyan Patel <akaypee26@gmail.com>
2026-05-05 16:23:43 +0530

    fix deploy setup

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ dotenv.config();
 
 const app = express();
 
+const path = require('path');
+
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -21,9 +23,19 @@ mongoose.connect(process.env.MONGO_URI)
 // Routes
 app.use('/api/auth', authRoutes);
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
