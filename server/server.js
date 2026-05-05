755728cf79236c5e686d41ff94d63c5731f6e163
Kalyan Patel <akaypee26@gmail.com>
2026-05-05 17:07:57 +0530

    fix express v5 routing

--- server/server.js
+++ server/server.js
@@ -26,11 +26,11 @@ app.use('/api/auth', authRoutes);
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+ app.use(express.static(path.join(__dirname, 'public')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
-  });
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 } else {
   app.get('/', (req, res) => {
     res.send('API is running');
